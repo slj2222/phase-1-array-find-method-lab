a8814cc378d15f469db7d16ff186326b9bb1e950
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-03 10:43:12 -0600

    commit tree 523b7b916cdedcff6dc1897c0b3cb6485c0a197a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(record) {
+
+    function isTrue(element) {
+        return(element.result === "W")
+    }//return record.find(isTrue)
+    const result = record.find(isTrue)
+    if (result === undefined) {
+        return undefined
+    } else {
+        return result.year;
+    }
+}
+superbowlWin(record)
+
+
